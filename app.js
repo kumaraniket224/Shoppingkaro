9648e629fb5cfa22d4fdfae5ade0d13617933145
kumaraniket224 <kumaraniket224@gmail.com>
2025-09-04 23:46:37 +0530

    h

--- app.js
+++ app.js
@@ -24,3 +24,37 @@ mode.addEventListener("click", () => {
     document.getElementById('imglogo').style.display="none"   }
  }
 
+const slides = document.querySelectorAll(".slide");
+var counter = 0;
+
+slides.forEach( 
+    (slide, index) => {
+        slide.style.left = `${index * 100}%`
+    }
+)
+
+
+const goprev = () => {
+
+    if(counter !=0){
+       counter--;
+    slideImage(); 
+    }
+    
+};
+
+const gonext = () =>{
+    if(counter < slides.length -1){
+        counter++;
+    slideImage();
+    }
+    
+};
+ 
+const slideImage = () => {
+ slides.forEach(
+    (slide) => {
+        slide.style.transform = `translateX(-${counter * 100}%)`
+    }
+ )
+};
